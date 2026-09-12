63ebf55bd3a93732f3d8f4876172ca486aafba92
Nguyễn Minh Tân <24110267@st.vju.ac.vn>
2026-09-12 10:46:03 +0700

    lập trình nâng cao

--- Program.cs
+++ Program.cs
@@ -1,38 +1,36 @@
-﻿//bai 1
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
-Console.WriteLine("Hello World!");
+Console.WriteLine("Hello, World!");
 int a;
 int b;
 int c;
-Console.Write("nhap a: ");
+Console.Write("Nhập a: ");
 a = Convert.ToInt32(Console.ReadLine());
+
 while (true)
 {
-  Console.Write("nhap b: ");
-  try
-  {
-    b = Convert.ToInt32(Console.ReadLine());
-    break;
-  }
-  catch (Exception)
-  {
-    Console.WriteLine("nhap sai, nhap lai");
-  }
+    Console.Write("Nhập b: ");
+
+    try
+    {
+        b = int.Parse(Console.ReadLine());
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Nhập sai! Vui lòng nhập lại: ");
+    }
 }
+
+bool k;
 while (true)
 {
-  Console.Write("nhap c: ");
-  bool k = int.TryParse(Console.ReadLine(), out c);
-  if (k)
-
-    break;
-  else
-    Console.WriteLine("nhap sai, nhap lai");
+    Console.Write("Nhập c: ");
+    k = int.TryParse(Console.ReadLine(), out c);
+    if (k)
+        break;
+    else
+        Console.WriteLine("Nhập sai! Vui lòng nhập lại: ");
 }
-
-
-
-Console.WriteLine($"a+b+c={a + b + c}");
+Console.WriteLine($"{a} + {b} + {c} = {a + b + c}");
